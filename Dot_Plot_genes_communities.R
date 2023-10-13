56fa04cedce35493dee8b8328f990d672d73f41a
Enrique Hernández-Lemus <ehernandez@inmegen.gob.mx>
2023-10-13 13:16:47 -0600

    Create Dot_Plot_genes_communities.R

---
+++ Dot_Plot_genes_communities.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+
+grafico_tesis <- read.csv("C:/Users/rodri/Downloads/Tabla para gráficos en R .csv")
+
+grafica_puntos <- ggplot(grafico_tesis, aes(x = Comunidad, y = Cantidad.de.genes, color = Subtipo.molecular)) +
+geom_point() +
+labs(title = "Cantidad de genes presentes por comunidad de cada subipo molecular",
+x = "Comunidad",
+y = "Cantidad de genes") + theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+scale_color_brewer(palette = "Set1")
+
+print(grafica_puntos)
